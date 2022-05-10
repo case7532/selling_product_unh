3c525c75155ec9e3a6bc00f1a0768e84c58aaad5
NuhVn22 <baitap.kho@gmail.com>
2022-05-10 21:48:22 +0700

    add lib and demo fragment

--- app/src/main/java/com/nuhvn/dung/MainActivity.kt
+++ app/src/main/java/com/nuhvn/dung/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.nuhvn.dung
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		supportFragmentManager.commit {
+			setReorderingAllowed(true)
+			replace(R.id.fragment_container, ProfileFragment())
+		}
 	}
 }
